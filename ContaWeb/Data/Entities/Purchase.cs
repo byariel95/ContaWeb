bc357ef188bb78a48fbd99bd03d651e668d062c3
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-07-03 17:06:39 -0600

    modified atributtes in purchase

--- ContaWeb/Data/Entities/Purchase.cs
+++ ContaWeb/Data/Entities/Purchase.cs
@@ -3,32 +3,42 @@ namespace ContaWeb.Data.Entities
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-    
+
 
     public class Purchase : IEntity
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(30, ErrorMessage = "El Campo {0} Solo Puede Contener {1} Caracteres")]
         [Display(Name = "Documento No.")]
         public string NoDocument { get; set; }
 
+        [Required]
         [Display(Name = "Fecha")]
         public DateTime Date { get; set; }
 
+        [Required]
+        [MaxLength(45, ErrorMessage = "El Campo {0} Solo Puede Contener {1} Caracteres")]
         [Display(Name = "Tipo De Documento")]
         public string Type { get; set; }
 
-        [Display(Name = "NIT")]
-        public  string Nit { get; set; }
 
+        [MaxLength(15, ErrorMessage = "El Campo {0} Solo Puede Contener {1} Caracteres")]
+        [Display(Name = "NIT")]
+        public string Nit { get; set; }
+
+        [Required]
         [Display(Name = "Valor Total")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Total { get; set; }
 
+        [Required]
         [Display(Name = "Precio Neto")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
 
+        [Required]
         [Display(Name = "Credito Fiscal")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal IVA { get; set; }
